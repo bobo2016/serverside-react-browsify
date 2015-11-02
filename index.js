847ac13d240c823a1ba76f1b3cf8f838f6edaded
Jingbo Zhang <jzhang@Tumis-MacBook-Pro.local>
2015-11-02 18:25:11 -0500

    Add demo code

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
 var React = require('react');
-React.render(<h1>Hello World</h1>, document.body);
+var ReactDOM = require('react-dom');
+
+var HelloWorld = React.createClass({
+	getInitialState : function(){
+		return {
+			name: "Charles"
+		}
+	},
+	render : function(){
+		var name =  this.state.name;
+		return (
+			<h1>Hello World, {name}</h1>
+		)
+	}
+})
+
+ReactDOM.render(<HelloWorld></HelloWorld>, document.getElementById('react-demo'));
 
 
